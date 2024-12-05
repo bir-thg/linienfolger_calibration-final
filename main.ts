1cb9c893f8a038f34b4f936a0d604c70a1b0be0f
Bernd <b.birkicht@thg-hn.de>
2024-12-06 00:20:09 +0100

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,15 @@
+function istZielErreicht () {
+	
+}
+input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
+    basic.showNumber(maqueen.readPatrol(maqueen.Patrol.PatrolLeft))
+    basic.pause(2000)
+    basic.clearScreen()
+})
+input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
+    basic.showNumber(maqueen.readPatrol(maqueen.Patrol.PatrolRight))
+    basic.pause(2000)
+    basic.clearScreen()
+})
+basic.setLedColor(0x0000ff)
+maqueen.setColor(0x0000ff)
